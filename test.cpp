45243c13bba21b0d4e4ef54369c117224adf9d2a
Clafout1s <clafou.prog@proton.me>
2026-02-07 22:39:44 +0100

    Fixed a bug where erasing caused some old rectangles to come on top

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@
 #include <math.h>
 #include <iomanip>
 #include <float.h>
-#include "rectangle.h"
+#include "vertexData.h"
 
 int sign(float num){
     if(num<0){
@@ -58,36 +58,35 @@ Point nearestTile(Point point,unsigned int size){
 }
 
 
-
-/*
-Polygone( (0.40000013, 0.11111110), (0.44999996, 0.11111110), (0.44999996, 0.02222216), (0.40000013, 0.02222216), (0.40000013, 0.11111110))
-Polygone( (0.39999995, 0.11111110), (0.44999996, 0.11111110), (0.44999996, 0.02222222), (0.39999995, 0.02222222), (0.39999995, 0.11111110))
-Polygone( (0.40000013, 0.11111110), (0.44999996, 0.11111110), (0.44999996, 0.02222222), (0.40000013, 0.02222222), (0.40000013, 0.11111110))
-
-*/
 int main()
 {
     srand(time(0));
-    std::cout << std::fixed << std::setprecision(8);
-    Rectangle target = Rectangle(Point(0.40000013, 0.11111110),Point(0.44999996, 0.11111110),Point(0.40000013, 0.02222216),Point(0.44999996, 0.02222216),{0,0,0});
-    Rectangle inter = Rectangle(Point(0.40000013, 0.11111110),Point(0.44999996, 0.11111110),Point(0.40000013, 0.02222222),Point(0.44999996, 0.02222222),{0,0,0});
-    std::cout<<(inter==target)<< " "<< fabs(0.02222216-0.02222222)<<"\n";
-    std::cout << (int)3.9999 <<"\n";
-    /*
-    Point pixel = Point(404.332, 352.836);
-    pixel = nearestTile(pixel,20);
-    printPoint(pixel);
-    std::cout << "\n";
-    Point norm = normalizePosition(pixel,800,800);
-    printPoint(norm);
-    std::cout << "\n";
-    Rectangle recNorm = calculateSquare(norm,20,800,1);
-    //std::cout << std::fixed << std::setprecision(5);
-    printRectangle(recNorm);
-    float epsilon = 0.00001;
-    if(abs(recNorm.top_left.x - 0) < epsilon){
-        printf("Zero !\n");
+    int nb = 5;
+    std::vector<Rectangle> rects = {};
+    std::vector<unsigned int> positions = {};
+    for (size_t i = 0; i < nb; i++)
+    {
+        rects.insert(rects.end(),Rectangle(Point(),rand()%20,{0,0,0}));
+        positions.insert(positions.end(),rand()%10);
     }
-    */
+    vertexData data = vertexData(rects);
+
+    std::cout <<"\n";
+    for (size_t i = 0; i < rects.size(); i++)
+    {
+        printRectGeogebra(rects[i]);
+        std::cout << positions[i]<<"\n";
+    }
+    std::cout << "\n";
+    std::tuple<std::vector<Rectangle>,std::vector<unsigned int>> sorted = data.sortRectArrayByPosition(rects,positions);
+    std::vector<Rectangle> sorted_rects = std::get<0>(sorted);
+    std::vector<unsigned int> sorted_pos = std::get<1>(sorted);
+    for (size_t i = 0; i < sorted_rects.size(); i++)
+    {
+        printRectGeogebra(sorted_rects[i]);
+        std::cout << sorted_pos[i]<<"\n";
+    }
+    
+     
     return 0;
 }
--- vertexData.h
+++ vertexData.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <algorithm>
+#include <cassert>
 #include "rectangle.h"
 
 class vertexData{
@@ -10,12 +11,25 @@ class vertexData{
         std::vector<Rectangle> rects;
         std::vector<unsigned int> indices;
 
+        
         vertexData(){}
         vertexData(Rectangle rec):vertexData(rec,std::vector<unsigned int>{0,1,2,1,2,3}){}
         vertexData(Rectangle s, std::vector<unsigned int> i): indices(i){
             rects.insert(rects.begin(),s);
         }
         vertexData(std::vector<Rectangle> s_list, std::vector<unsigned int> i): rects(s_list), indices(i){}
+        vertexData(std::vector<Rectangle> s_list): rects(s_list){
+            std::vector<unsigned int> indices_one_rec = {0,1,2,1,2,3};
+            unsigned int old_index_max_value=0;
+            for (size_t i = 0; i < s_list.size(); i++)
+            {
+                for(size_t y=0;y<indices_one_rec.size();y++){
+                    indices.push_back(indices_one_rec[y] + 1 + old_index_max_value);
+                }
+                old_index_max_value = indices[indices.size()-1];
+            }
+        }
+
 
         vertexData& operator+=(const vertexData& v2){
             if(this->rects.empty() && this->indices.empty()){
@@ -32,12 +46,39 @@ class vertexData{
                 //eraseRectangles((Rectangle&)v2.rects[i]);
                 rects.push_back(v2.rects[i]);
             }
-            unsigned int old_max_i = indices[indices.size()-1];
-            for(size_t i=0;i<v2.indices.size();i++){
-                indices.push_back(v2.indices[i] + 1 + old_max_i);
+            unsigned int old_index_max_value = indices[indices.size()-1];
+            for(size_t y=0;y<v2.indices.size();y++){
+                indices.push_back(v2.indices[y] + 1 + old_index_max_value);
             }
             return *this;
         };
+
+        
+        vertexData& addAtPosition(const vertexData& v2,std::vector<unsigned int> positions){
+            assert(positions.size()==v2.rects.size());
+            if(this->rects.empty() && this->indices.empty()){
+                this->rects = v2.rects;
+                this->indices = v2.indices;
+                return *this;
+            }
+            
+            std::tuple<std::vector<Rectangle>,std::vector<unsigned int>> sorted_arrays = sortRectArrayByPosition(v2.rects,positions);
+            std::vector<Rectangle> v2_rects_sorted = std::get<0>(sorted_arrays);
+            std::vector<unsigned int> positions_sorted = std::get<1>(sorted_arrays);
+            std::vector<unsigned int> new_indices = {0,1,2,1,2,3};
+            unsigned int old_index_max_value=0;
+            for (size_t i = 0; i < v2.rects.size(); i++)
+            {
+                this->rects.insert(this->rects.begin()+positions_sorted[i],v2_rects_sorted[i]);
+
+                old_index_max_value = indices[indices.size()-1];
+                for(size_t y=0;y<new_indices.size();y++){
+                    indices.push_back(new_indices[y] + 1 + old_index_max_value);
+                }
+            }
+
+            return *this;
+        }
 
         
         vertexData& clear(){
@@ -75,6 +116,7 @@ class vertexData{
         bool eraseRectangles(Rectangle& eraser){
             std::vector<int> erase_list = {};
             std::vector<Rectangle> add_list = {};
+            std::vector<unsigned int> add_list_positions = {};
             for (size_t i = 0; i < rects.size(); i++)
             {
                 std::vector<Rectangle> result = cutFromRectangle(rects[i],eraser);
@@ -84,10 +126,9 @@ class vertexData{
 
                         if(!result[y].isNull()){
                            add_list.insert(add_list.end(),result[y]);
+                           add_list_positions.insert(add_list_positions.end(),i);
                         }
-                        
                     }
-                        
                 }
             }
             std::sort(erase_list.begin(),erase_list.end(),std::greater<int>()); // Sort descending, to avoid issues in deletion indexes
@@ -97,13 +138,49 @@ class vertexData{
                 rects.erase(rects.begin()+erase_list[e]);
                 indices.erase(indices.end()-6,indices.end());
             }
+
+            vertexData new_rects_data = vertexData(add_list);
+            /*
             for (size_t a = 0; a < add_list.size(); a++)
             {
                 std::vector<unsigned int> indices_new = {0,1,2,1,2,3};
-                *this+=vertexData(add_list[a],indices_new);
-            }
+                this->addAtPosition(vertexData(add_list[a],indices_new),{add_list_positions[a]});
+            }*/
+
+            if(!add_list.empty()) this->addAtPosition(new_rects_data,add_list_positions);
+            //this->addAtPosition(new_rects_data,add_list_positions);
+ 
             return !erase_list.empty() && !add_list.empty();
         }
-        
+
+
+    //private:
+        std::tuple<std::vector<Rectangle>,std::vector<unsigned int>> sortRectArrayByPosition(const std::vector<Rectangle> vec_rects, const std::vector<unsigned int> positions){
+            std::vector<Rectangle> vec_rects_copy = vec_rects;
+            std::vector<Rectangle> new_vec_rects={};
+            std::vector<unsigned int> positions_copy = positions;
+            std::vector<unsigned int> new_positions = {};
+            
+            size_t smallest_pos_i = 0;
+            for (size_t y = 0; y < positions.size(); y++)
+            {
+               for (size_t i = 1; i < positions_copy.size(); i++)
+                {
+                    if(positions_copy[i]<positions_copy[smallest_pos_i]){
+                        smallest_pos_i = i;
+                    }
+                }
+                new_positions.insert(new_positions.end(),positions_copy[smallest_pos_i]);
+                new_vec_rects.insert(new_vec_rects.end(),vec_rects_copy[smallest_pos_i]);
+                positions_copy.erase(positions_copy.begin()+smallest_pos_i);
+                vec_rects_copy.erase(vec_rects_copy.begin()+smallest_pos_i);
+                smallest_pos_i = 0;
+            }
+            
+            std::tuple<std::vector<Rectangle>,std::vector<unsigned int>> result ={
+                new_vec_rects,new_positions
+            };
+            return result;
+        }
 };
 #endif
